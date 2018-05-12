b2049019d4b37774f236b5b1a1a1753d1115cfbc
Antti Kupila <akupila@gmail.com>
2018-05-12 11:02:13 +0200

    Add automatic release with goreleaser

--- cmd/gitprompt/gitprompt.go
+++ cmd/gitprompt/gitprompt.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/akupila/gitprompt"
+)
+
+var (
+	version   = "dev"
+	commit    = "none"
+	date      = "unknown"
+	goversion = "unknown"
 )
 
 const defaultFormat = "#B([@b#R%h][#y ›%s][#m ↓%b][#m ↑%a][#r x%c][#g +%m][#y %u]#B)"
@@ -34,8 +43,73 @@ func (f *formatFlag) String() string {
 
 var format formatFlag
 
+var exampleStatus = &gitprompt.GitStatus{
+	Branch:    "master",
+	Sha:       "0455b83f923a40f0b485665c44aa068bc25029f5",
+	Untracked: 1,
+	Modified:  2,
+	Staged:    3,
+	Conflicts: 4,
+	Ahead:     5,
+	Behind:    6,
+}
+
+var formatHelp = func() string {
+	return strings.TrimSpace(fmt.Sprintf(`
+Define output format.
+
+Default format is: %q
+Example result:    %s
+
+Data:
+	%%h	Current branch or SHA1
+	%%s	Number of files staged
+	%%b	Number of commits behind remote
+	%%a	Number of commits ahead of remote
+	%%c	Number of conflicts
+	%%m	Number of files modified
+	%%u	Number of untracked files
+
+Colors:
+	#k	Black
+	#r	Red
+	#g	Green
+	#y	Yellow
+	#b	Blue
+	#m	Magenta
+	#c	Cyan
+	#w	White
+	#K	Highlight Black
+	#R	Highlight Red
+	#G	Highlight Green
+	#Y	Highlight Yellow
+	#B	Highlight Blue
+	#M	Highlight Magenta
+	#C	Highlight Cyan
+	#W	Highlight White
+
+Text attributes:
+	@b	Set bold
+	@B	Clear bold
+	@f	Set faint/dim color
+	@F	Clear faint/dim color
+	@i	Set italic
+	@I	Clear italic
+`, defaultFormat, gitprompt.Print(exampleStatus, defaultFormat)))
+}
+
 func main() {
+	v := flag.Bool("version", false, "Print version inforformation.")
 	flag.Var(&format, "format", formatHelp())
 	flag.Parse()
+
+	if *v {
+		fmt.Printf("Version:    %s\n", version)
+		fmt.Printf("Commit:     %s\n", commit)
+		fmt.Printf("Build date: %s\n", date)
+		fmt.Printf("Go version: %s\n", goversion)
+		os.Exit(0)
+	}
+
 	gitprompt.Exec(format.String())
 }
